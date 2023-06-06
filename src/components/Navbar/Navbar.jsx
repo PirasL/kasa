1b8888beff32e444ded76d8fda26c3b1959c8bee
PirasL <leo.piras@outlook.com>
2023-06-06 15:21:41 +0200

    Ajout underline active nav menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/images/logo-kasa.png";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function Navbar() {
+  const activePage = window.location.pathname;
+  console.log(activePage)
   return (
     <header>
       <img src={logo} alt="logo Kasa"/>
       <nav className="nav-wrapper">
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A Propos</Link>
+        <Link to="/" className={`${activePage === "/" ? "activeMenu" : ""}`}>Accueil</Link>
+        <Link to="/about" className={`${activePage === "/about" ? "activeMenu" : ""}`}>A Propos</Link>
       </nav>
     </header>
   );
